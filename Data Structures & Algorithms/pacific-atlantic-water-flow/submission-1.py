ff8a74ffff05ca8a78d3b78d1a7bffcffcfd3e98
mous9270 <140340980+mous9270@users.noreply.github.com>
2026-04-03 16:51:27 +0530

    Update: pacific-atlantic-water-flow - submission-1

--- Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -7,36 +7,36 @@ class Solution:
         sp = set() # Pacific reachable
         sa = set() # Atlantic reachable
         
-        def dfs(r, c, visit_set, prev_height):
+        def dfs(i, j, visit_set, prev_height):
             # 1. Check boundaries
             # 2. Check if already visited to prevent infinite loops
             # 3. Check if current height is >= previous (moving uphill)
-            if (r < 0 or c < 0 or r == m or c == n or 
-                (r, c) in visit_set or heights[r][c] < prev_height):
-                return
-            
-            visit_set.add((r, c))
-            
-            # Explore neighbors
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            for dr, dc in directions:
-                dfs(r + dr, c + dc, visit_set, heights[r][c])
+            # if (r < 0 or c < 0 or r == m or c == n or 
+            #     (r, c) in visit_set or heights[r][c] < prev_height):
+            #     return
+            if 0<=i<m and 0<=j<n and (i,j) not in visit_set and heights[i][j]>=prev_height:
+                visit_set.add((i, j))
+                
+                # Explore neighbors
+                for a,b in [(0,1), (0,-1),(1,0), (-1,0)]:
+                    r,c=i+a, j+b
+                    dfs(r,c,visit_set,heights[i][j])
 
         # Start DFS from the left and right edges (Rows)
-        for r in range(m):
-            dfs(r, 0, sp, heights[r][0])       # Left edge (Pacific)
-            dfs(r, n - 1, sa, heights[r][n - 1]) # Right edge (Atlantic)
+        for i in range(m):
+            dfs(i, 0, sp, heights[i][0])       # Left edge (Pacific)
+            dfs(i, n - 1, sa, heights[i][n - 1]) # Right edge (Atlantic)
             
         # Start DFS from the top and bottom edges (Columns)
-        for c in range(n):
-            dfs(0, c, sp, heights[0][c])       # Top edge (Pacific)
-            dfs(m - 1, c, sa, heights[m - 1][c]) # Bottom edge (Atlantic)
+        for j in range(n):
+            dfs(0, j, sp, heights[0][j])       # Top edge (Pacific)
+            dfs(m - 1, j, sa, heights[m - 1][j]) # Bottom edge (Atlantic)
             
         # The answer is the intersection of both sets
         ans = []
-        for r in range(m):
-            for c in range(n):
-                if (r, c) in sp and (r, c) in sa:
-                    ans.append([r, c])
+        for i in range(m):
+            for j in range(n):
+                if (i, j) in sp and (i, j) in sa:
+                    ans.append([i, j])
                     
         return ans
